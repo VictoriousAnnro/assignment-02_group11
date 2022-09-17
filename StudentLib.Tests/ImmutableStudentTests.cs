50c613916bb29e607c354ce37f120778d8d7f17b
Anne-Marie <annemarierommerdahl@gmail.com>
2022-09-17 18:00:57 +0200

    added equality tester for record students

--- StudentLib.Tests/ImmutableStudentTests.cs
+++ StudentLib.Tests/ImmutableStudentTests.cs
@@ -3,12 +3,15 @@ using static StudentLib.Status;
 
 public class ImmutableStudentTests
 {
+
     [Fact]
-    public void St()
+    public void Equality_comparer_tester()
     {
-        ImmutableStudent s = new(1, "Mikkel", "Johnsen", new DateTime(2008, 5, 1, 8, 30, 52), new DateTime(2011, 5, 1, 8, 30, 52), new DateTime(2014, 5, 1, 8, 30, 52));
-        string st = "Mikkel Johnsen 1 Dropout";
-        Assert.Equal(st, s.ToString());
+        ImmutableStudent s = new(1, "Mikkel", "Johnsen", Dropout, new DateTime(2008, 5, 1, 8, 30, 52), new DateTime(2011, 5, 1, 8, 30, 52), new DateTime(2014, 5, 1, 8, 30, 52));
+        ImmutableStudent s2 = new(1, "Mikkel", "Johnsen", Dropout, new DateTime(2008, 5, 1, 8, 30, 52), new DateTime(2011, 5, 1, 8, 30, 52), new DateTime(2014, 5, 1, 8, 30, 52));
+        ImmutableStudent s3 = new(3, "Morten", "Jensen", Graduated, new DateTime(2009, 5, 1, 8, 30, 52), new DateTime(2012, 5, 1, 8, 30, 52), new DateTime(2012, 5, 1, 8, 30, 52));
+        Assert.Equal(s, s2);
+        Assert.NotEqual(s, s3);
     }
 
     //mangler at lave ordentlige tests
