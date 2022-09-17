2830ba0beb9da7911af82a09785c8b4f66bf742e
Anne-Marie <annemarierommerdahl@gmail.com>
2022-09-17 18:00:05 +0200

    added first delegates test

--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -3,8 +3,24 @@ namespace Assignment2.Tests;
 public class DelegatesTests
 {
     [Fact]
-    public void Test1()
+    public void reverseString_tester()
     {
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        
+        Reverse r = delegate(string line){
+            foreach(char c in line.Reverse()){
+                Console.Write(c);
+            }
+        };
+        r("hello this is a test!");
+        
+        string reversed = "!tset a si siht olleh";
 
-    }
+        Assert.Equal(reversed, stringWriter.ToString());
+        }
+
+
+
+        public delegate void Reverse(string line);
 }
