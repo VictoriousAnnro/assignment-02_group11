745736150d17dc8a944ced75db4342ac65ef1057
Anne-Marie <annemarierommerdahl@gmail.com>
2022-09-18 16:28:48 +0200

    made complete anon.method tests

--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -18,7 +18,36 @@ public class DelegatesTests
         string reversed = "!tset a si siht olleh";
 
         Assert.Equal(reversed, stringWriter.ToString());
-        }
+    }
+
+    [Fact]
+    public void Number_product_tester()
+    {
+        var product = (double n1, double n2) => n1*n2;
+
+        var expected37 = 3.5*7.01;
+        var expected35 = 3.1*5.9;
+
+        Assert.Equal(expected37, product(3.5, 7.01));
+        Assert.Equal(expected35, product(3.1, 5.9));
+
+    }
+
+    [Fact]
+    public void Number_and_string_equal_tester()
+    {
+        var areEqual = (string line, int number) => {
+            int lineNumber = int.Parse(line);
+            if(lineNumber == number){
+                return true;
+            } return false;
+        };
+
+        Assert.True(areEqual("42", 42));
+        Assert.True(areEqual(" 0042", 42));
+        Assert.False(areEqual("13", 3));
+
+    }
 
 
 
